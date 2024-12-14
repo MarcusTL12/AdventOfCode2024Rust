4f0cb87a951284b1f1e31caed17623764a75db49
MarcusTL12 <marcuspaafjellet@gmail.com>
2024-12-14 16:46:38 +0100

    Day 14

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,7 @@
 use ndarray::{Array2, ArrayView2, s};
 
+use num_integer::Integer;
+
 fn linelen(input: &[u8]) -> usize {
     input
         .iter()
@@ -25,4 +27,11 @@ pub fn input_to_grid_owned(input: Vec<u8>) -> Array2<u8> {
     Array2::from_shape_vec([h, w + 1], input)
         .unwrap()
         .slice_move(s![.., 0..w])
+}
+
+pub fn crt(a1: i64, n1: i64, a2: i64, n2: i64) -> i64 {
+    let gcd = i64::extended_gcd(&n1, &n2);
+    let x = (a2 * n1 * gcd.x + a1 * n2 * gcd.y) % (n1 * n2);
+
+    if x < 0 { x + n1 * n2 } else { x }
 }
