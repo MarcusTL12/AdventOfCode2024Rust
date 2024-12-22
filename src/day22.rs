94c9479c52002a29f405ca12e0465f0d2850a80d
MarcusTL12 <marcuspaafjellet@gmail.com>
2024-12-22 07:43:09 +0100

    Revert "parallel simd"
    This reverts commit 4c945400dbdd378b2c0440dc59582dc01c717057.

--- src/day22.rs
+++ src/day22.rs
@@ -1,9 +1,5 @@
 use std::simd::Simd;
 
-use rayon::{
-    iter::ParallelIterator, slice::ParallelSlice, str::ParallelString,
-};
-
 use crate::{Day, TaskResult};
 
 pub const PARTS: Day = [part1, part2];
@@ -16,7 +12,7 @@ fn evolve(mut secret: u32) -> u32 {
     secret
 }
 
-const N: usize = 16;
+const N: usize = 8;
 
 fn evolve_simd(mut secret: Simd<u32, N>) -> Simd<u32, N> {
     secret = ((secret << 6) ^ secret) & Simd::splat(0xffffff);
@@ -27,34 +23,34 @@ fn evolve_simd(mut secret: Simd<u32, N>) -> Simd<u32, N> {
 }
 
 fn part1(input: String) -> TaskResult {
-    let input: Vec<u32> =
-        input.par_lines().map(|l| l.parse().unwrap()).collect();
+    let mut it = input.lines().map(|l| l.parse().unwrap()).array_chunks();
 
-    input
-        .par_chunks(N)
-        .map(|c| {
-            if c.len() == N {
-                let mut v = Simd::from_slice(c);
-
-                for _ in 0..2000 {
-                    v = evolve_simd(v);
-                }
-
-                v.to_array().into_iter().map(|x| x as u64).sum()
-            } else {
-                c.iter()
-                    .cloned()
-                    .map(|mut n| {
-                        for _ in 0..2000 {
-                            n = evolve(n);
-                        }
-                        n as u64
-                    })
-                    .sum::<u64>()
+    let main_sum = it
+        .by_ref()
+        .flat_map(|n| {
+            let mut n = Simd::from_array(n);
+            for _ in 0..2000 {
+                n = evolve_simd(n);
             }
+            n.to_array()
         })
-        .sum::<u64>()
-        .into()
+        .map(|x| x as u64)
+        .sum::<u64>();
+
+    let rest_sum = it
+        .into_remainder()
+        .map(|rest| {
+            rest.map(|mut n| {
+                for _ in 0..2000 {
+                    n = evolve(n);
+                }
+                n as u64
+            })
+            .sum::<u64>()
+        })
+        .unwrap_or(0);
+
+    (main_sum + rest_sum).into()
 }
 
 fn part2(input: String) -> TaskResult {
